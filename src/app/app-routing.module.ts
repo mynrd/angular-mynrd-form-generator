fa3f2a67283ea11a6a0d3390b436f95cc2dcd0c7
mynrd <mynrd@live.com>
2019-12-12 10:36:46 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormGeneratorComponent } from './form-generator/form-generator.component';
-import { LayoutSampleComponent } from './layout-sample/layout-sample.component';
-
 
 const appRoutes: Routes = [
   { path: '', component: FormGeneratorComponent },
